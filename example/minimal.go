dd49b0de18194d9b57530cffd0f50ed5afb5d2e5
Ryan Halloran <ryan.halloran.4@gmail.com>
2019-12-09 15:11:28 -0800

    minor modifications to example/minimal.go

--- example/minimal.go
+++ example/minimal.go
@@ -5,25 +5,35 @@ import (
 	"net/http"
 	"strings"
 
+	"github.com/rhallora-heidelberg/handle/respondwith"
+
 	"github.com/julienschmidt/httprouter"
 	"github.com/rhallora-heidelberg/handle"
 )
 
-func Hello(r *http.Request, _ httprouter.Params) handle.Response {
+// "Hello World" using handle.Response directly
+func Hello_direct(r *http.Request, _ httprouter.Params) handle.Response {
 	return handle.Response{
 		StatusCode: http.StatusOK,
 		Body:       strings.NewReader("Hello, World!"),
 	}
 }
 
-func Empty(r *http.Request, _ httprouter.Params) handle.Response {
-	return handle.Response{}
+// "Hello World" using respondwith
+func Hello_sugary(r *http.Request, _ httprouter.Params) handle.Response {
+	return respondwith.Stringf("Hello, World!")
+}
+
+// basic 404
+func NotFound(r *http.Request, _ httprouter.Params) handle.Response {
+	return respondwith.StatusCode(http.StatusNotFound)
 }
 
 func main() {
 	router := httprouter.New()
-	router.GET("/", handle.With(Hello))
-	router.GET("/bad", handle.With(Empty))
+	router.GET("/hello1", handle.With(Hello_direct))
+	router.GET("/hello2", handle.With(Hello_sugary))
+	router.GET("/hl3releasedate", handle.With(NotFound))
 
 	log.Println("Listening on port 8000")
 	log.Fatal(http.ListenAndServe(":8000", router))
